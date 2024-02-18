6f263cfc6b71e0dffa897d1a43711473b1ca598c
Sandip Senapati <SandipSuman761@gmail.com>
2024-02-18 20:23:34 +0530

    Spring 1st commit

---
+++ MyLocalretailer/src/main/java/com/my1sttimeSring/MyLocalretailer/MyLocalretailerApplication.java
@@ -0,0 +1,13 @@
+package com.my1sttimeSring.MyLocalretailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLocalretailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLocalretailerApplication.class, args);
+	}
+
+}
---
+++ MyLocalretailer/src/test/java/com/my1sttimeSring/MyLocalretailer/MyLocalretailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my1sttimeSring.MyLocalretailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLocalretailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
